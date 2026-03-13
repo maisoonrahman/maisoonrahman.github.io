02ee7972a4711be54861e024383b2dedcd250006
maisoonrahman <maisoon.a.r01@gmail.com>
2026-03-13 08:59:44 -0400

    added linkedin badge to contact card

--- portfolio/src/components/extras/ConnectCard.jsx
+++ portfolio/src/components/extras/ConnectCard.jsx
@@ -9,18 +9,19 @@ export default function ConnectCard() {
       className="xl:col-span-2"
     >
 
-      <div className="flex flex-col gap-4">
+      <div className="text-xs flex flex-col gap-4">
 
-        <p>
-          Always open to interesting projects and conversations.
-        </p>
+       
 
-        <a
-          href="mailto:maisoon.a.rahman@gmail.com"
-          className="bg-pink-400 text-white px-4 py-2 rounded-xl w-fit"
-        >
-          Book a Chat
-        </a>
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+
+       <div class="badge-base LI-profile-badge" 
+       data-locale="en_US" data-size="medium" 
+       data-theme="light" data-type="VERTICAL" 
+       data-vanity="maisoon-rahman" data-version="v1">
+        <a class="badge-base__link LI-simple-link" 
+        href="https://www.linkedin.com/in/maisoon-rahman?trk=profile-badge"></a></div>
+              
 
       </div>
 
